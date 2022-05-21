365301848c63ef241a0fff17cd708e2deba0c4a5
Yuriy <batm1x1@gmail.com>
2022-05-21 22:23:17 +0300

    add class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,52 +1,34 @@
 import React from "react"
 import UserItem from "./UserItem";
 import s from './Users.module.css'
-import axios from "axios";
 
-class Users extends React.Component{
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.activePage}&count=${this.props.usersOnPage}`).then(response => {
-
-            this.props.showUsers(response.data.items)
-            this.props.updateTotalUsers(response.data.totalCount)
-        })
+const Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsers / props.usersOnPage)
+    let pages = []
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
     }
+    let curP = props.activePage
+    let curPF = ((curP - 3) < 0) ? 0 : curP - 3
+    let curPL = curP + 3
+    let slicedPages = pages.slice(curPF, curPL)
 
-    getUsersOnPage = (n) => {
-        this.props.setActivePage(n)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${n}&count=${this.props.usersOnPage}`).then(response => {
-            this.props.showUsers(response.data.items)
-        })
-    }
-
-    render(){
-        let pagesCount = Math.ceil(this.props.totalUsers/this.props.usersOnPage)
-        let pages = []
-        for(let i=1; i <= pagesCount; i++){
-            pages.push(i)
-        }
-        let curP = this.props.activePage;
-        let curPF = ((curP - 3) < 0) ?  0  : curP - 3 ;
-        let curPL = curP + 3;
-        let slicedPages = pages.slice( curPF, curPL);
-
-        return (
+    return (
+        <div>
             <div>
-                <div>
-                    {slicedPages.map(n=> <span onClick={()=>this.getUsersOnPage(n)} className={this.props.activePage === n && s.active}>{n}</span>)}
-                </div>
-                <div className={s.user}>
-                    { this.props.users.map(n=> <UserItem name={n.name}
-                                                    id={n.id}
-                                                    photo={n.photos.large}
-                                                    followed={n.followed}
-                                                    status={n.status}
-                                                    pushFollow={this.props.pushFollow} />)}
-                </div>
+                {slicedPages.map(n => <span onClick={() => props.getUsersOnPage(n)}
+                                            className={props.activePage === n && s.active}>{n}</span>)}
             </div>
-        )
-    }
+            <div className={s.user}>
+                {props.users.map(n => <UserItem name={n.name}
+                                                     id={n.id}
+                                                     photo={n.photos.large}
+                                                     followed={n.followed}
+                                                     status={n.status}
+                                                     pushFollow={props.pushFollow}/>)}
+            </div>
+        </div>
+    )
 }
-
 export default Users
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,9 +1,42 @@
 import {connect} from "react-redux";
 import Users from "./Users";
 import {followToggle, setActivePageAC, setUsers, totalUsersAC} from "../../redux/usersReducer";
+import React from "react";
+import axios from "axios";
 
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.activePage}&count=${this.props.usersOnPage}`).then(response => {
+
+            this.props.showUsers(response.data.items)
+            this.props.updateTotalUsers(response.data.totalCount)
+        })
+    }
+
+    getUsersOnPage = (n) => {
+        this.props.setActivePage(n)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${n}&count=${this.props.usersOnPage}`).then(response => {
+            this.props.showUsers(response.data.items)
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                  <Users totalUsers={this.props.totalUsers}
+                         usersOnPage={this.props.usersOnPage}
+                         activePage={this.props.activePage}
+                         users={this.props.users}
+                         pushFollow={this.props.pushFollow}
+                         getUsersOnPage={this.getUsersOnPage}
+                  />
+            </div>
+        )
+    }
+}
 const mapStateToProps = (state) => {
-
     return {
         users: state.userList.users,
         totalUsers: state.userList.totalUsers,
@@ -19,5 +52,4 @@ const dispatchStateToProps = (dispatch) =>{
         updateTotalUsers: (userCount) => dispatch(totalUsersAC(userCount))
     }
 }
-const UsersContainer = connect(mapStateToProps, dispatchStateToProps)(Users)
-export default UsersContainer
+export default connect(mapStateToProps, dispatchStateToProps)(UsersContainer)
