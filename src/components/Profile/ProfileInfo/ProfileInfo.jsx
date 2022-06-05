83d213a8ddeea027d4d4501024bd78e5d03b8a61
Yuriy <batm1x1@gmail.com>
2022-06-06 01:38:11 +0300

    profile info reduction

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,28 +2,44 @@ import React from "react"
 import s from './ProfileInfo.module.css'
 import ProfileStatus from "./ProfileStatus";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+import userDefaultPhoto from '../../../assets/images/personal-user.png'
 
 
 
 const ProfileInfo = ({currentProfile, status, updateMyStatus, isShowMyProfile, handlePhotoChange }) => {
-
+    let Contact = ({socialMedia, contactValue} ) => {
+        return <div>
+                <b>{socialMedia}:</b>{contactValue}
+        </div>
+    }
     const OnPhotoSelected = (onChange) => {
-        handlePhotoChange(onChange.target.files[0])
+        if(onChange.target.files[0]) handlePhotoChange(onChange.target.files[0])
     }
     return (
         <div>
             <div>
-                <img src={currentProfile.photos.large}/>
+                <img src={currentProfile.photos.large || userDefaultPhoto}/>
             </div>
             <div>
                 {!isShowMyProfile && <input type={"file"} onChange={OnPhotoSelected}/>}
             </div>
                 <ProfileStatusWithHooks status={status} updateMyStatus={updateMyStatus}/>
-            <div>
-                <span>{currentProfile.aboutMe}</span>
-            </div>
             <div className={s.descriptionBlock}>
                 <span>{currentProfile.fullName}</span>
+            </div>
+            <div>
+                <b>About me:</b> {currentProfile.aboutMe}
+            </div>
+            <div>
+                <b>Looking for a job:</b> {currentProfile.lookingForAJob ? "yes" : "no"}
+            </div>
+            <div>
+                {currentProfile.lookingForAJob && <div><b>Looking for a job description:</b> {currentProfile.lookingForAJobDescription}</div>}
+            </div>
+            <div>
+                <b>Contacts:</b> {Object.keys(currentProfile.contacts).map(key => {
+               return  <Contact key={key} socialMedia={key} contactValue={currentProfile.contacts[key]}/>
+            })}
             </div>
         </div>
     )
