67496a13396a9fcca51aab703a9d0afa17d9692d
Yuriy <batm1x1@gmail.com>
2022-07-29 20:19:51 +0300

    animated on hover avatar

---
+++ src/components/HOOK/useDebounce.js
@@ -0,0 +1,15 @@
+import {Dispatch, SetStateAction, useCallback, useRef} from "react";
+
+export default function useDebounce(callback: Dispatch<SetStateAction<boolean>>, delay: number) {
+    const timer = useRef<ReturnType<typeof setInterval>>()
+    const debouncedCallback = useCallback((...args:any[]) => {
+        if(timer.current) {
+            clearTimeout(timer.current)
+        }
+        timer.current = setTimeout(() => {
+            callback(...args)
+        }, delay)
+    }, [callback, delay])
+
+    return debouncedCallback
+}
